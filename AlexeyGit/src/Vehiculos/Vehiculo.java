53baf3cdd2b798adaa246cc89141c4716bb7c912
Alexey <alexeysemionov@hotmail.com>
2019-05-29 21:18:28 +0200

    2 commit

---
+++ AlexeyGit/src/Vehiculos/Vehiculo.java
@@ -0,0 +1,15 @@
+package Vehiculos;
+
+public class Vehiculo {
+	
+	private int numPuertas;
+	private int numRuedas;
+
+	public Vehiculo( int numPuertas, int numRuedas){
+		this.numPuertas = numPuertas;
+		this.numRuedas = numRuedas;
+	}
+
+}
+
+
